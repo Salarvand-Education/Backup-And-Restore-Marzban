2bf5177d76059b6ba62992e5a9c611b070147ef2
Salarvand-Education <amirsam@salarvand.org>
2024-07-07 18:24:55 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+cd Backup-And-Restore-Marzban
 
-# Prompting the user for the bot token and admin chat ID
 read -p "Please enter your bot token: " BOT_TOKEN
 read -p "Please enter your admin chat ID: " ADMIN_CHAT_ID
 
-# Creating or overwriting the .env file
 cat <<EOL > .env
 BOT_TOKEN=$BOT_TOKEN
 ADMIN_CHAT_ID=$ADMIN_CHAT_ID
